d88cf616b6b43570f1ee50b3aa59785aadb4af56
Agustin G <agiannastasio@gmail.com>
2019-07-18 20:47:03 -0300

    Updates

---
+++ precache-manifest.f59fffc3dad52fce007fb687dd84c9f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239e1484952ac07f856beab5f2e1f05a",
+    "url": "/agiannastasio/react-todolist/index.html"
+  },
+  {
+    "revision": "6489b992e87176684fe9",
+    "url": "/agiannastasio/react-todolist/static/css/main.d13f3255.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/agiannastasio/react-todolist/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "6489b992e87176684fe9",
+    "url": "/agiannastasio/react-todolist/static/js/main.424eb809.chunk.js"
+  },
+  {
+    "revision": "eeef872809b935f3feb6",
+    "url": "/agiannastasio/react-todolist/static/js/runtime~main.0ea95afe.js"
+  }
+]);
