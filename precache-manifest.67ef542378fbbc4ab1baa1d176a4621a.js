0de4f769741c0dea1fd3c189215c9641904fac59
Agustin G <agiannastasio@gmail.com>
2019-07-18 21:15:43 -0300

    Updates

---
+++ precache-manifest.67ef542378fbbc4ab1baa1d176a4621a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d798f519e8bde35d130d1710bfde80",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "6489b992e87176684fe9",
+    "url": "/react-todolist/static/css/main.d13f3255.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/react-todolist/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "6489b992e87176684fe9",
+    "url": "/react-todolist/static/js/main.424eb809.chunk.js"
+  },
+  {
+    "revision": "dd2a0c730e02a59d4852",
+    "url": "/react-todolist/static/js/runtime~main.8b2e420c.js"
+  }
+]);
